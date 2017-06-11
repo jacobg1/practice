ba17a24197f556bc76b120e17ffe069f6ab51321
jacobg1 <jeg4t@virginia.edu>
2017-06-11 18:44:06 -0500

    MORE js

---
+++ Dev/JavaScript/morearraypractice.js
@@ -0,0 +1,8 @@
+var contacts = ["Matt Smith", "sam davis", "ashley jones"];
+var dad = contacts[1];
+contacts[0] = "mark williams";
+contacts.pop();
+contacts;
+contacts.push("Ashley Johnson");
+contacts;
+var numberOfContacts = contacts.length
